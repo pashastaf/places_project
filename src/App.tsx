6679cbebea0ce0992fe4c3a3bb4255a47af08897
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-04-18 20:39:32 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.module.css'
+import { AppDispatch, RootState } from './states/store';
+import { changeTest } from './states/restPlace';
 
 export default function App() {
+  const testStore = useSelector((state: RootState) => state.test)
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <>
-    
+      {testStore.test && testStore.test.map((el,index) => {
+        return(
+          <button onClick={() => dispatch(changeTest({index:index}))} key={index}>{el}</button>
+        )
+      })}
     </>
   )
 }
